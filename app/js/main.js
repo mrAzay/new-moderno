422d0a98068ff04dca4d245f7b9406a936414472
mrAzay <furoxaz@gmail.com>
2019-10-17 18:02:55 +0300

    finish

---
+++ app/js/main.js
@@ -0,0 +1,94 @@
+$(function () {
+
+
+  if ($('.products__inner-box').length) {
+    var mixer = mixitup('.products__inner-box');
+  }
+  else {
+  }
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+    spacing: "2px"
+  });
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToScroll: 4,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+    ]
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+
+  });
+
+  $('.icon-th-list').on('click', function () {
+    $('.product-page__content .product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+
+  $('.icon-th-large').on('click', function () {
+    $('.product-page__content .product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
+
+  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function (event) {
+    var id = $(this).attr('data-id');
+    $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
+  $('input[type="file"], select').styler();
+
+});
